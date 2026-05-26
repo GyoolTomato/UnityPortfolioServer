7882ce16d709aba59c529f6f94a3ad5ec79b670a
RohTaeyoung <40551068+RohTaeyoung@users.noreply.github.com>
2026-05-26 20:41:53 +0900

    웹훅 업로드

---
+++ UPServer_Webhook.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    console.log('Push detected');
+
+    exec(`
+        cd /home/gyools/UnityPortfolioServer &&
+        git pull &&
+        pm2 restart UPServer
+    `, (err, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+    });
+
+    res.send('OK');
+});
+
+app.listen(3001, () => {
+    console.log('Webhook server running');
+});
